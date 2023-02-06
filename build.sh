725232baab55d4adf93ecaad1fb307be86826f5e
Dalton Herrewynen <neuronmaker@gmail.com>
2023-02-06 12:11:21 -0700

    Added the sample build shell script

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# will compile the main.cpp file into its calculator sample app referencing the classes I defined in this project
+g++ -o calculator main.cpp ArbitraryPrecisionInt.cpp ArbitraryPrecisionFloat.cpp
